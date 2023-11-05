b4675d0fca37e5f813d5b0e51e3f80aa1af0c02d
theodora <theodora.tosheva@gmail.com>
2023-11-05 17:54:46 +0200

    Add description to user roles

--- src/main/java/nbu/bg/logisticscompany/model/entity/UserRole.java
+++ src/main/java/nbu/bg/logisticscompany/model/entity/UserRole.java
@@ -1,8 +1,13 @@
 package nbu.bg.logisticscompany.model.entity;
 
 public enum UserRole {
+    // Roles of employees/staff of the company
     COURIER,
     OFFICE_EMPLOYEE,
+
+    // Role of admin of the company
     ADMIN,
+
+    // Role of sender/receiver of orders
     CLIENT
 }
