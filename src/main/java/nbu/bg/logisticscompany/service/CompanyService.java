d462baa3c31282cbb010cc02f5b17345017b03f5
Victor Sotirov <victorsotirov@mail.bg>
2023-11-10 17:42:28 +0200

    added comment in service to remind me

--- src/main/java/nbu/bg/logisticscompany/service/CompanyService.java
+++ src/main/java/nbu/bg/logisticscompany/service/CompanyService.java
@@ -1,6 +1,7 @@
 package nbu.bg.logisticscompany.service;
 
 //Might need to move it to the matching repository class
+//DA PITAM ZA KOETO
 public interface CompanyService
 {
     void updateCompanyName(Long companyId, String newCompanyName);
