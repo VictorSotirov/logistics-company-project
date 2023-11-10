f824f03496d02c295a7a8e18322c688e6c877469
Victor Sotirov <victorsotirov@mail.bg>
2023-11-10 17:39:48 +0200

    initial company implementatin

--- src/main/java/nbu/bg/logisticscompany/service/CompanyService.java
+++ src/main/java/nbu/bg/logisticscompany/service/CompanyService.java
@@ -1,4 +1,13 @@
 package nbu.bg.logisticscompany.service;
 
-public interface CompanyService {
+//Might need to move it to the matching repository class
+public interface CompanyService
+{
+    void updateCompanyName(Long companyId, String newCompanyName);
+
+    void updateCompanyAddress(Long companyId, String newCompanyAddress);
+
+    void deleteCompanyById(Long companyId);
+
+    void deleteCompanyByName(String name);
 }
--- src/main/java/nbu/bg/logisticscompany/service/impl/CompanyServiceImpl.java
+++ src/main/java/nbu/bg/logisticscompany/service/impl/CompanyServiceImpl.java
@@ -1,8 +1,43 @@
 package nbu.bg.logisticscompany.service.impl;
 
+import lombok.AllArgsConstructor;
+import nbu.bg.logisticscompany.model.entity.Company;
+import nbu.bg.logisticscompany.repository.CompanyRepository;
 import nbu.bg.logisticscompany.service.CompanyService;
 import org.springframework.stereotype.Service;
 
+//Potential refactor to use the only available company
 @Service
-public class CompanyServiceImpl implements CompanyService {
+@AllArgsConstructor
+public class CompanyServiceImpl implements CompanyService
+{
+    private final CompanyRepository companyRepository;
+
+    @Override
+    public void updateCompanyName(Long companyId, String newCompanyName)
+    {
+        Company company = companyRepository.findById(companyId).orElse(null);
+
+        if (company != null)
+        {
+            company.setName(newCompanyName);
+
+            companyRepository.save(company);
+        }
+    }
+
+    @Override
+    public void updateCompanyAddress(Long companyId, String newCompanyAddress) {
+
+    }
+
+    @Override
+    public void deleteCompanyById(Long companyId) {
+
+    }
+
+    @Override
+    public void deleteCompanyByName(String name) {
+
+    }
 }
