d4612392155ddfa14fd8f136eae1e1642b1a1042
Victor Sotirov <victorsotirov@mail.bg>
2024-02-02 21:32:45 +0200

    Changed the company name to be dynamically displayed in the footer and header

---
+++ src/main/java/nbu/bg/logisticscompany/controller/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package nbu.bg.logisticscompany.controller;
+
+import lombok.AllArgsConstructor;
+import nbu.bg.logisticscompany.service.CompanyService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+@AllArgsConstructor
+public class GlobalControllerAdvice
+{
+    private final CompanyService companyService;
+
+    @ModelAttribute
+    public void addAttributes(Model model)
+    {
+        companyService.getCompanyData().ifPresent(companyData -> {
+            String companyName = companyData.getName();
+            model.addAttribute("companyName", companyName);
+        });
+    }
+
+}
