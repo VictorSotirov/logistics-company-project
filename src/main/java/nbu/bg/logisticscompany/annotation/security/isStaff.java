e71bd75fd9c5eb99a988cd184ee0932427d7ac34
dnny <jlpopov13@gmail.com>
2024-02-07 10:03:39 +0200

    add admin role to isStaff

--- src/main/java/nbu/bg/logisticscompany/annotation/security/isStaff.java
+++ src/main/java/nbu/bg/logisticscompany/annotation/security/isStaff.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('COURIER') or hasAuthority('OFFICE_EMPLOYEE')")
+@PreAuthorize("hasAuthority('COURIER') or hasAuthority('OFFICE_EMPLOYEE') or hasAuthority('ADMIN')")
 public @interface isStaff {
 }
